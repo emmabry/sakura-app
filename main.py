1ce7d1f698a9b84a63b117fea27eb30c8cfc53de
emma <emmaabryan@gmail.com>
2024-11-05 17:30:32 +0000

    Reorganise files, add vocab functionality

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from dotenv import load_dotenv
-import os
-from googletrans import Translator
-from gtts import gTTS
-import pytesseract
-from PIL import Image
 from flask import Flask, render_template, request
+from img_to_speech import get_speech, img_to_text, translate
+from vocab import get_entries
 
-load_dotenv()
-
-VOICE_ID = 'j210dv0vWm7fCknyQpbA'
-XI_API_KEY = os.getenv('XI_API_KEY')
-TESSDATA_PREFIX = os.getenv('TESSDATA_PREFIX')
-
-os.environ['TESSDATA_PREFIX'] = TESSDATA_PREFIX
-
-
-translator = Translator()
 app = Flask(__name__)
 
 
@@ -36,24 +22,10 @@ def convert_image():
         return render_template('tospeech.html', translation=translated_text, image=file_path, audio=audio)
     return render_template('tospeech.html')
 
-
-def get_speech(requested_text):
-    tts = gTTS(requested_text, lang='ja')
-    with open('static/speech.mp3', 'wb') as f:
-        tts.write_to_fp(f)
-    return tts
-
-
-def img_to_text():
-    image = Image.open('static/to_translate.png')
-    text = pytesseract.image_to_string(image, lang='jpn')
-    return text
-
-
-def translate(img_txt):
-    result = translator.translate(text=img_txt, src="ja", dest="en")
-    return result.text
-
+@app.route('/vocab', methods=['GET'])
+def get_vocab():
+    vocab_list = get_entries()
+    return render_template('vocab.html', vocab_list=vocab_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
